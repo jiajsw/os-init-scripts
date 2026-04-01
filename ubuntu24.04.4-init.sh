31bc89741a34afaeaae404f6075cc4daa2ff7d6a
jsw <954951185@qq.com>
2026-04-02 00:29:42 +0800

    添加脚本以卸载 snap 并安装必要的软件包

---
+++ ubuntu24.04.4-init.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# 卸载 snap 
+sudo apt autoremove --purge snapd
+
+sudo rm -rvf /tmp/snap-private-tmp/snap.snapd-desktop-integration/tmp/.snap/snap
+sudo rm -rvf /home/jsw/snap
+sudo rm -rvf /sys/fs/bpf/snap
+sudo rm -rvf /root/snap
+sudo rm -rvf /var/lib/gdm3/snap
+sudo rm -rvf ~/snap
+
+cat <<EOF | sudo tee /etc/apt/preferences.d/nosnap.pref > /dev/null
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+
+Package: snap-confine
+Pin: release a=*
+Pin-Priority: -10
+
+Package: ubuntu-core-launcher
+Pin: release a=*
+Pin-Priority: -10
+EOF
+
+sudo apt install vim -y
+# 设置行号 /etc/vim/vimrc
+echo 'set nu' | sudo tee -a /etc/vim/vimrc
+
+# 安装 gnome-software
+sudo apt install --no-install-recommends gnome-software -y
+
+sudo locale-gen en_US.UTF-8 zh_CN.UTF-8 
+
+sudo apt install -y flatpak gnome-software-plugin-flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
+
+flatpak update
